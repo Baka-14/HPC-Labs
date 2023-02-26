985a033531343d214765e1e3b66e7932b212b928
Baka <ventrapragadasaisrikar123@gmail.com>
2023-02-26 11:56:49 +0530

    parallelised threshold code

--- Lab-3/threshold.c
+++ Lab-3/threshold.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include<math.h> 
-#include<limits.h>
-#include<string.h>
+#include <sys/time.h>
+#include <math.h> 
+#include <limits.h>
+#include <string.h>
+#include <omp.h>
 
-void generate(int r,int c,int **arr){ 
+
+void generate(int r, int c, int **arr) {
     srand(time(NULL));
-    for(int i=0;i<r;i++)
-    { 
-        for(int j=0;j<c;j++)
-        {
-            arr[i][j]=rand() % 301;
+    for (int i = 0; i < r; i++) { 
+        for (int j = 0; j < c; j++) {
+            arr[i][j] = rand() % 301;
         }
     }
-} 
+}
 
-int check(int r, int c,int **arr,int x, int per) {
-    int count=0;
-    for(int i=0;i<r;i++)
-    {   
-        for(int j=0;j<c;j++)
-        {
-            if(arr[i][j]>x)
-            {
-                count=count+1;
-                if(count>per)
+int check(int r, int c, int **arr, int x, int per) {
+    int count = 0;
+    for (int i = 0; i < r; i++) {   
+        for (int j = 0; j < c; j++) {
+            if (arr[i][j] > x) {
+                count = count + 1;
+                if (count > per)
                     return count;
             }
         } 
@@ -33,77 +30,69 @@ int check(int r, int c,int **arr,int x, int per) {
     return count;
 }
 
-
-void threshold(int p,int r, int c,int** arr,int** img){
-    int prod = r*c*p;
-    int x=prod/100;
+void threshold(int p, int r, int c, int **arr, int **img) {
+    int prod = r * c * p;
+    int x = prod / 100;
     int k; 
-    // int min=INT_MIN;
-    
-    for(int i=0;i<r;i++)
-    {   int count=0;
-        for(int j=0;j<c;j++)
-        { 
+
+    #pragma omp parallel for shared(r,c,arr,img,x) private(k) num_threads(6)
+    for (int i = 0; i < r; i++) {   
+        for (int j = 0; j < c; j++) {
             k = arr[i][j];
-            // if(k>min){
-            //     img[i][j]=0;
-            //     min=k;
-            //     continue;
-            // }
-            count=check(r, c, arr, k, x);
-            printf("%d ",count);
-             if(count<=x)
-            {
-                img[i][j]=1;
+            int count = check(r, c, arr, k, x);
+            // printf("%d ",count);
+            if (count <= x) {
+                img[i][j] = 1;
             } 
-            else
-            {
-                img[i][j]=0;
+            else {
+                img[i][j] = 0;
             }
         } 
-        printf("\n");
+        // printf("\n");
     }
 }
 
-
-void print(int r, int c,int **arr){
-   for(int i=0;i<r;i++)
-    { 
-        for(int j=0;j<c;j++)
-        {
-            printf("%d ",arr[i][j]);
+void print(int r, int c, int **arr) {
+    for (int i = 0; i < r; i++) { 
+        for (int j = 0; j < c; j++) {
+            printf("%d ", arr[i][j]);
         } 
         printf("\n");
     }
 } 
 
-int main(int argc,char* argv[]){
+int main(int argc, char* argv[]) {
     // argv 1 is size of matrix and 2 is percentage of matrix
-    int r=5;
-    int c=5;
-    int p=10;
+    int r = atoi(argv[1]);
+    int c = atoi(argv[2]);
+    int p = 10;
+
     int **matrix;
-    matrix = (int **)malloc(r * sizeof(int *));
+    matrix = (int **) malloc(r * sizeof(int *));
     // making it two dimensional
-    for (int row = 0; row < r; row++)
-    {
-        matrix[row] = (int *)malloc(c * sizeof(int));
+    for (int row = 0; row < r; row++) {
+        matrix[row] = (int *) malloc(c * sizeof(int));
     }  
-
     int **bin_img;
-    bin_img = (int **)malloc(r * sizeof(int *));
+    bin_img = (int **) malloc(r * sizeof(int *));
     // making it two dimensional
-    for (int row = 0; row < r; row++)
-    {
-        bin_img[row] = (int *)malloc(c * sizeof(int));
+    for (int row = 0; row < r; row++) {
+        bin_img[row] = (int *) malloc(c * sizeof(int));
     } 
+    struct timeval tv1, tv2;
+    struct timezone tz;
+    double elapsed; 
+    
 
-    generate(r,c,matrix);  
-    print(r,c,matrix); 
-    printf("-----------------------------------------");
-    printf("\n");
-    threshold(p,r,c,matrix,bin_img); 
-    printf("-----------------------------------------");
-    printf("\n");
-    print(r,c,bin_img);
+    generate(r, c, matrix);  
+    printf("Original matrix:\n");
+    print(r, c, matrix); 
+    printf("-----------------------------------------\n");
+    gettimeofday(&tv1, &tz);
+    threshold(p, r, c, matrix, bin_img); 
+    gettimeofday(&tv2, &tz);
+    elapsed = (double) (tv2.tv_sec-tv1.tv_sec) + (double) (tv2.tv_usec-tv1.tv_usec) * 1.e-6;
+    printf("%4.2lf\n",elapsed);
+    printf("Binary image:\n");
+    print(r, c, bin_img);
 }
