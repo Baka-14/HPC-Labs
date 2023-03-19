a3a2a4b02fb6d4ba6b6cfd6b3aa8c0a33667e976
aditya-sake <70888527+aditya-sake@users.noreply.github.com>
2023-03-19 14:08:52 +0530

    Add files via upload
    Double linked list optimistic list

--- Lab-4/ListImplementations/OptimisticList.java
+++ Lab-4/ListImplementations/OptimisticList.java
@@ -1,110 +1,140 @@
 /* OptimisticList.java */
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-public class OptimisticList{
+
+public class OptimisticList {
   private Node head;
+  
   public OptimisticList() {
-    this.head  = new Node(Integer.MIN_VALUE);
-    this.head.next = new Node(Integer.MAX_VALUE);
+    this.head = new Node(Integer.MIN_VALUE, null, new Node(Integer.MAX_VALUE, null, null));
+    this.head.next.prev = this.head;
   }
+  
   public boolean add(int item) {
     int key = item;
     while (true) {
       Node pred = this.head;
       Node curr = pred.next;
       while (curr.key <= key) {
-        pred = curr; curr = curr.next;
+        pred = curr;
+        curr = curr.next;
       }
-      pred.lock(); curr.lock();
+      pred.lock();
+      curr.lock();
       try {
         if (validate(pred, curr)) {
           if (curr.key == key) { // present
             return false;
-          } else {               // not present
-            Node node = new Node(item);
-            node.next = curr;
+          } else { // not present
+            Node node = new Node(item, pred, curr);
+            curr.prev = node;
             pred.next = node;
             return true;
           }
         }
-      } finally {                // always unlock
-        pred.unlock(); curr.unlock();
+      } finally { // always unlock
+        pred.unlock();
+        curr.unlock();
       }
     }
   }
-  /* Remove an element.*/
+  
+  /* Remove an element. */
   public boolean remove(int item) {
     int key = item;
     while (true) {
       Node pred = this.head;
       Node curr = pred.next;
       while (curr.key < key) {
-        pred = curr; curr = curr.next;
+        pred = curr;
+        curr = curr.next;
       }
-      pred.lock(); curr.lock();
+      pred.lock();
+      curr.lock();
       try {
         if (validate(pred, curr)) {
           if (curr.key == key) { // present in list
-            pred.next = curr.next;
+            curr.prev.next = curr.next;
+            curr.next.prev = curr.prev;
             return true;
-          } else {               // not present in list
+          } else { // not present in list
             return false;
           }
         }
-      } finally {                // always unlock
-        pred.unlock(); curr.unlock();
+      } finally { // always unlock
+        pred.unlock();
+        curr.unlock();
       }
     }
   }
-  /*Test whether element is present*/
+  
+  /* Test whether element is present */
   public boolean contains(int item) {
     int key = item;
     while (true) {
       Node pred = this.head; // sentinel node;
       Node curr = pred.next;
       while (curr.key < key) {
-        pred = curr; curr = curr.next;
+        pred = curr;
+        curr = curr.next;
       }
       try {
-        pred.lock(); curr.lock();
+        pred.lock();
+        curr.lock();
         if (validate(pred, curr)) {
           return (curr.key == key);
         }
-      } finally {                // always unlock
-        pred.unlock(); curr.unlock();
+      } finally { // always unlock
+        pred.unlock();
+        curr.unlock();
       }
     }
   }
+  
   private boolean validate(Node pred, Node curr) {
-    Node node = head;
-    while (node.key <= pred.key) {
-      if (node == pred)
-        return pred.next == curr;
-      node = node.next;
-    }
-    return false;
+    return !pred.isDeleted() && !curr.isDeleted() && pred.next == curr && curr.prev == pred;
   }
-public void display()
-{
-	Node temp=head;
-	while(temp.next!=null){
-		System.out.print("\t"+temp.key);
-		temp=temp.next;
-	}
-}
-/**
-* list entry
-*/
-private class Node {
+
+  public void display() {
+    Node temp = head;
+    while (temp != null) {
+      System.out.print("\t" + temp.key);
+      temp = temp.next;
+    }
+  }
+
+  /**
+   * list entry
+   */
+  private class Node {
     int key;
     Node next;
+    Node prev;
+    boolean deleted;
     Lock lock;
-    Node(int item) {
+
+    Node(int item, Node prev, Node next) {
       this.key = item;
-      lock = new ReentrantLock();
-      this.next=null;
+      this.prev = prev;
+      this.next = next;
+      this.deleted = false;
+      this.lock = new ReentrantLock();
     }
-    void lock() {lock.lock();}
-    void unlock() {lock.unlock();}
+
+    void lock() {
+      lock.lock();
+    }
+
+    void unlock() {
+      lock.unlock();
+    }
+
+    boolean isDeleted() {
+      return deleted;
+    }
+
+    void setDeleted(boolean deleted) {
+      this.deleted = deleted;
+    }
   }
 }
